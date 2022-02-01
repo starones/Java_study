b79fdfb6c370e9c949becec7149e5156ee4503f1
田中一星 <tanakaissei@tanakahajimeseinoMacBook-Pro.local>
2022-02-01 15:44:43 +0900

    [java 4章]

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "スーパーマン";
+		final int NUM_1 = 1;
+		System.out.println(name);
+		
+		name = "帝一";
+		System.out.println(name);
+		
+		System.out.println(NUM_1);
+	}
+}
