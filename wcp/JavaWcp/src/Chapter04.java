f8921effff2d0725546d7d26b0d558175a2e06f3
starones <itstarone172@gmail.com>
2022-02-01 15:50:36 +0900

    [Fix]java4章

--- wcp/JavaWcp/src/Chapter04.java
+++ wcp/JavaWcp/src/Chapter04.java
@@ -1,11 +1,11 @@
 
 public class Chapter04 {
 	public static void main(String[] args) {
-		String name = "スーパーマン";
+		String name = "真面目";
 		final int NUM_1 = 1;
 		System.out.println(name);
 		
-		name = "帝一";
+		name = "チャラチャラ";
 		System.out.println(name);
 		
 		System.out.println(NUM_1);
