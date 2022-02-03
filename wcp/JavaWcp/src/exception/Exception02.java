abe715dc4ee60276b4d332bae9477515f19d5d10
starones <itstarone172@gmail.com>
2022-02-03 14:45:08 +0900

    [java 12章]

--- wcp/JavaWcp/src/exception/Exception02.java
+++ wcp/JavaWcp/src/exception/Exception02.java
@@ -4,7 +4,7 @@ public class Exception02 {
 	public static void main(String[] args) {
 		try {
 			System.out.println("100 ÷ 0は?");
-			int result = 100 / 2;
+			int result = 100 / 0;
 			System.out.println("計算結果" + result);
 		} catch (ArithmeticException e) {
 			System.out.println("例外が発生");
