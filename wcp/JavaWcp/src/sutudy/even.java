34bc948d7285c39dbb73c57e67fa7116ae724856
starones <itstarone172@gmail.com>
2022-03-04 17:12:56 +0900

    [Add]lesson1_for文

--- wcp/JavaWcp/src/sutudy/even.java
+++ wcp/JavaWcp/src/sutudy/even.java
@@ -3,10 +3,20 @@ package sutudy;
 //1から10の偶数表示
 public class even {
 	public static void main(String[] args) {
+		//---------for文-------
 		for (int number = 1; number <= 10; number++) {
 			if (number % 2 == 0) {
 				System.out.println(number);
 			}
+		}
+
+		//--------wile文--------
+		int num = 0;
+		while (num <= 10) {
+			num++;
+				if (num % 2 == 0) {
+					System.out.println(num);
+				}
 		}
 	}
 }
