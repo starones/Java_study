ba4507d243c983b01089f3c4f02427a0e3b4b828
starones <itstarone172@gmail.com>
2022-03-04 14:32:59 +0900

    [Add]java_lesson

---
+++ wcp/JavaWcp/src/sutudy/even.java
@@ -0,0 +1,13 @@
+package sutudy;
+
+public class even {
+	public static void main(String[] args) {
+		for (int number = 1; number <= 10; number++) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+		
+	}
+
+}
