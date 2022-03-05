4e5e92b4015b1f0f382d48433efbb8f2e05f3016
starones <itstarone172@gmail.com>
2022-03-05 16:38:41 +0900

    [Add]double

---
+++ wcp/JavaWcp/src/rogate/Double.java
@@ -0,0 +1,12 @@
+package rogate;
+
+public class Double {
+	public static void main(String[] args) {
+		double num1 = 8.5,
+			   num2 = 3.4;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 * num2);
+	}
+
+}
