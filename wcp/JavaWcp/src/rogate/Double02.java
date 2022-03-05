3f1d6c809cd46b26b4935ec5ac59c6426e435f01
starones <itstarone172@gmail.com>
2022-03-05 17:24:15 +0900

    [Add]キャスト

---
+++ wcp/JavaWcp/src/rogate/Double02.java
@@ -0,0 +1,14 @@
+package rogate;
+
+public class Double02 {
+	//キャストする
+	public static void main(String[] args) {
+		int num1 = 7,
+			num2 = 2;
+		System.out.println(num1 / num2);
+
+		//double型にキャスト
+		System.out.print((double)num1 / num2);
+	}
+
+}
