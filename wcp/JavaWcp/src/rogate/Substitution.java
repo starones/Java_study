a292f26768e5a3fb0fab83a1579eb54577082f56
starones <itstarone172@gmail.com>
2022-03-05 15:48:10 +0900

    [Add]Substitution02

---
+++ wcp/JavaWcp/src/rogate/Substitution.java
@@ -0,0 +1,13 @@
+package rogate;
+
+public class Substitution {
+	public static void main(String[] args) {
+		int number = 3;
+		System.out.println(number);
+		
+		number = number + 7;
+		
+		System.out.println(number);
+	}
+
+}
