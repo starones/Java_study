bb93455b56b2cb3cab4f81d8880013581dfc95af
starones <itstarone172@gmail.com>
2022-03-06 14:31:31 +0900

    [Add]boolean

---
+++ wcp/JavaWcp/src/rogate/Boolean.java
@@ -0,0 +1,15 @@
+package rogate;
+
+public class Boolean {
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println(12 / 4 == 3);
+		System.out.println(12 / 4 != 3);
+		
+		boolean bool = 3 * 9 == 27;
+		System.out.println(bool);
+		
+	}
+
+}
