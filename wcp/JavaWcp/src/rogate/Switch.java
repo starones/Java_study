aa9f175a2665f852d0292608a1fc18d51fa8917a
starones <itstarone172@gmail.com>
2022-03-06 15:56:02 +0900

    [Add]Switch文

---
+++ wcp/JavaWcp/src/rogate/Switch.java
@@ -0,0 +1,18 @@
+package rogate;
+
+public class Switch {
+	public static void main(String[] args) {
+		int n = 1;
+
+		switch (n) {
+		case 1:
+			System.out.println("大吉だお");
+			break;
+
+		case 2:
+			System.out.println("吉だお");
+			break;
+		}
+	}
+
+}
