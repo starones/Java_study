4640321a56ffdb050ec06ef3d6314b6a4abff41a
starones <itstarone172@gmail.com>
2022-03-07 17:11:26 +0900

    [Add]メソッドと引数

---
+++ wcp/JavaWcp/src/rogate/Method02.java
@@ -0,0 +1,14 @@
+package rogate;
+
+//---------メソッドの引数--------
+public class Method02 {
+	public static void main(String[] args) {
+		printDate("マッチョ");
+		printDate("ガリガリ");
+	}
+
+	public static void printDate(String name) {
+		System.out.println(name);
+	}
+
+}
