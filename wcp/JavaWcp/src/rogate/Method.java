48f2bb0a95a306c20b25a3ca819a7e8a5c3f303c
starones <itstarone172@gmail.com>
2022-03-07 16:02:09 +0900

    [Add]メソッド

---
+++ wcp/JavaWcp/src/rogate/Method.java
@@ -0,0 +1,12 @@
+package rogate;
+//------------メソッドについて----------
+public class Method {
+	public static void main(String[] args) {
+		hello();
+	}
+
+	public static void hello() {
+		System.out.println("Hello Java world");
+	}
+
+}
