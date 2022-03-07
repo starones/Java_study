d0458408f94b848b7fa4d0f23ee9afa8f00bc64c
starones <itstarone172@gmail.com>
2022-03-07 12:26:54 +0900

    [Add]配列

---
+++ wcp/JavaWcp/src/rogate/Arrangement.java
@@ -0,0 +1,11 @@
+package rogate;
+//-------------配列---------------
+public class Arrangement {
+	public static void main(String[] args) {
+		String[] names = {"わんわん", "にゃんにゃん", "オロロロ"};
+		
+		System.out.println(names[0]);
+		System.out.println(names[2]);
+	}
+
+}
