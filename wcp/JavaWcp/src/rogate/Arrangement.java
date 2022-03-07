b03c7d84a5b181ae7582dfc34006b33edbccbf81
starones <itstarone172@gmail.com>
2022-03-07 14:39:37 +0900

    [Add]配列の繰り返し文

--- wcp/JavaWcp/src/rogate/Arrangement.java
+++ wcp/JavaWcp/src/rogate/Arrangement.java
@@ -6,6 +6,21 @@ public class Arrangement {
 		
 		System.out.println(names[0]);
 		System.out.println(names[2]);
+		
+		
+		//---------配列の上書き------------
+		// 変数languagesを定義し、配列を代入してください
+	    String[] languages = {"Ruby", "PHP", "Python"};
+	    
+	    // インデックス番号が「1」の要素を出力してください
+	    System.out.println(languages[1]);
+	    
+	    // インデックス番号が「1」の要素を「Java」で上書きしてください
+	    languages[1] = "Java";
+	    
+	    // インデックス番号が「1」の要素を出力してください
+	    System.out.println(languages[1]);
+	    
 	}
 
 }
---
+++ wcp/JavaWcp/src/rogate/Arrangement_for.java
@@ -0,0 +1,12 @@
+package rogate;
+//-----------配列と繰り返し文------------
+public class Arrangement_for {
+	public static void main(String[] args) {
+		String[] names = {"猫", "わし", "犬"};
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
