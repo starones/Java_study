584fc63ac3334bea070b1f82c88eb745ab66f748
starones <itstarone172@gmail.com>
2022-03-08 23:00:07 +0900

    [Fix]配列

--- wcp/JavaWcp/src/sutudy/Lesson03.java
+++ wcp/JavaWcp/src/sutudy/Lesson03.java
@@ -1,12 +1,20 @@
 package sutudy;
 
 public class Lesson03 {
-	 public static void main(String[] args) {
-		    int number[] = new int [20];
-		    for (int i = 0; i <  number.length; i++) {
-		    	int number2 = number[i] + 5;
-		    	number[i] = number2;
-		      System.out.print( number[i]);
-		    }
-		  }
+	public static void main(String[] args) {
+	    int number[] = new int[20];
+
+	    // nnumberの表示
+	    for (int i = 1; i < number.length; i++) {
+	    	if (i >=11 && !(i % 2 == 0)) {
+
+	    		System.out.print(",");
+	    	}
+	    	if (i < 11 && i % 2 == 0) {
+
+	    		System.out.print(",");
+	    	}
+	    	System.out.print(String.format("%d", i, number[i]));
+	    }
+	  }
 }
