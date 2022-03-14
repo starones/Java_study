afb27aa5a88149ddaf946745134da41a0a2b9c68
starones <itstarone172@gmail.com>
2022-03-14 23:10:57 +0900

    Fix

--- wcp/JavaWcp/src/sutudy/Test04.java
+++ wcp/JavaWcp/src/sutudy/Test04.java
@@ -3,18 +3,12 @@ package sutudy;
 public class Test04 {
 	public static void main(String[] args) {
 		int num[] = new int[20];
-
-		for (int i = 1; i < num.length; i++) {
-			System.out.print(String.format("%d", i,  num[i]));
-
-			if(i <= 10 && i % 2 != 0) {
-				System.out.print(",");
-			}
-			if(i > 10 && i % 2 == 0) {
-				System.out.print(",");
-			}
+		
+		num[0] = 0;
+		System.out.println(num[0]);
+		
+		
 		}
 
 	}
 
-}
